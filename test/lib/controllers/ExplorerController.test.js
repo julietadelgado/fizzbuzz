53c6bbd1cc83a4ffdd310b06e2d64e7dbd1ec8f7
Julieta Delgado <julidmeza@gmail.com>
2022-04-26 23:23:29 -0500

    Refactor code

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,8 +1,8 @@
-const ExplorerController = require("./../../../lib/controllers/ExplorerController")
+const ExplorerController = require("./../../../lib/controllers/ExplorerController");
 
 describe("Test for ExplorerController", () => {
     test("1. Validate if returns list of explorers with mission node", () => {
-        const explorersInNode = ExplorerController.getExplorersByMission("node")
+        const explorersInNode = ExplorerController.getExplorersByMission("node");
         expect(explorersInNode).toEqual(
             expect.arrayContaining([
                 expect.objectContaining({mission: "node"})
