654d715a7a1de8d08a6c7ddc7fa5a3bcf56059ad
Julieta Delgado <julidmeza@gmail.com>
2022-04-26 19:27:43 -0500

    Write functional code for method filterByMission - ExplorerService

--- test/lib/utils/services/ExplorerService.test.js
+++ test/lib/utils/services/ExplorerService.test.js
@@ -1,3 +1,6 @@
+const Reader = require("./../../../../lib/utils/Reader")
+const ExplorerService = require("./../../../../lib/utils/services/ExplorerService")
+
 describe("Test for ExplorerService", () => {
     test("1. Validate if filter array has node mission", () => {
         const explorers = Reader.readJsonFile("./explorers.json")
